c2ac3a3d3ce7eb4845dc9eb2e47c6617c869e04f
Brittney <brittney.postma@hotmail.com>
2020-05-13 23:21:15 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Particles from 'react-particles-js'
-import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
 import SignIn from './components/SignIn/SignIn'
 import Register from './components/Register/Register'
@@ -11,88 +10,133 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import { particlesOptions } from './particlesOptions'
 import './App.css'
 
-const app = new Clarifai.App({
-	apiKey: process.env.REACT_APP_API_KEY
-})
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			input: '',
-			imageUrl: '',
-			box: {},
-			route: 'signin',
-			isSignedIn: false
-		}
-	}
+  constructor() {
+    super()
+    this.state = initialState
+  }
 
-	onRouteChange = route => {
-		if (route === 'signout') {
-			this.setState({ isSignedIn: false })
-		} else if (route === 'home') {
-			this.setState({ isSignedIn: true })
-		}
-		this.setState({ route: route })
-	}
+  loadUser = data => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
 
-	calculateFaceLocation = data => {
-		const clarifaiFace =
-			data.outputs[0].data.regions[0].region_info.bounding_box
-		const image = document.getElementById('inputImage')
-		const width = Number(image.width)
-		const height = Number(image.height)
-		return {
-			leftCol: clarifaiFace.left_col * width,
-			topRow: clarifaiFace.top_row * height,
-			rightCol: width - clarifaiFace.right_col * width,
-			bottomRow: height - clarifaiFace.bottom_row * height
-		}
-	}
+  onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState(initialState)
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route })
+  }
 
-	displayFaceBox = box => {
-		this.setState({ box })
-	}
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    }
+  }
 
-	onInputChange = e => {
-		this.setState({ input: e.target.value })
-	}
+  displayFaceBox = box => {
+    this.setState({ box })
+  }
 
-	onButtonSubmit = () => {
-		this.setState({ imageUrl: this.state.input })
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then(res => this.displayFaceBox(this.calculateFaceLocation(res)))
-			.catch(err => console.log(err))
-	}
+  onInputChange = e => {
+    this.setState({ input: e.target.value })
+  }
 
-	render() {
-		const { isSignedIn, imageUrl, route, box } = this.state
-		return (
-			<div className='App'>
-				<Particles className='particles' params={particlesOptions} />
-				<Navigation
-					isSignedIn={isSignedIn}
-					onRouteChange={this.onRouteChange}
-				/>
-				{route === 'home' ? (
-					<div>
-						<Logo />
-						<Rank />
-						<ImageLinkForm
-							onButtonSubmit={this.onButtonSubmit}
-							onInputChange={this.onInputChange}
-						/>
-						<FaceRecognition box={box} imageUrl={imageUrl} />
-					</div>
-				) : route === 'signin' ? (
-					<SignIn onRouteChange={this.onRouteChange} />
-				) : (
-					<Register onRouteChange={this.onRouteChange} />
-				)}
-			</div>
-		)
-	}
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onButtonSubmit={this.onButtonSubmit}
+              onInputChange={this.onInputChange}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === 'signin' ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import App from './App'
 import 'tachyons'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.unregister()
 
